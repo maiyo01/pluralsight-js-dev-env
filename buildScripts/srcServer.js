3537b4098aaaa41d43bfb7263f3f041c27ede4fd
maiyo01 <maiyokevin@gmail.com>
2019-07-09 20:05:13 +0300

    eslint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
