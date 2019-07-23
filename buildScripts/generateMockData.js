226d0e0794cfcbe1f53a918e9194dae68e12ef92
maiyo01 <maiyokevin@gmail.com>
2019-07-24 00:27:04 +0300

    http_requet/appveyor

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+
+import jsf from 'json-schema-faker';
+import {schema} from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
+import faker from "faker";
+
+jsf.extend("faker", function(){
+  return faker;
+});
+
+var outputFile = './src/api/db.json';
+
+//const json = JSON.stringify(jsf(schema))
+
+jsf.resolve(schema).then(function(result){
+  fs.writeFile(outputFile, JSON.stringify(result, null, 2), function(err){
+    if (err){
+      return console.log(err);
+    }
+    else
+    {
+      console.log(chalk.green(`Mock Data Generated Here: ${outputFile}`));
+    }
+  });
+});
+
+
+
